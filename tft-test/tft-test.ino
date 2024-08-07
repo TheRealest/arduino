5f98ece6470d717713b35d520debb2e55942f940
Réal Provencher-Deshler <real.provencher.deshler@gmail.com>
2024-08-07 00:19:00 -0400

    removed unused code

--- tft-test/tft-test.ino
+++ tft-test/tft-test.ino
@@ -37,52 +37,6 @@ void setup() {
   uiStateManager.start();
 }
 
-//int8_t rotaryCounter1 = 0;
-//
-//void handleDebugEncoder1(RotaryEncoder& encoder) {
-//  int8_t rotaryStateChange = encoder.getRotaryStateChange();
-//  int8_t switchStateChange = encoder.getSwitchStateChange();
-//
-//  if (rotaryStateChange != 0) {
-//    rotaryCounter1 += rotaryStateChange;
-//    if (rotaryCounter1 < 0) {
-//      rotaryCounter1 = 0;
-//    }
-//    if (rotaryCounter1 > 0xFF) {
-//      rotaryCounter1 = 0xFF;
-//    }
-//    screen.testHexPair(UI_INITIAL_OFFSET, UI_INITIAL_OFFSET, rotaryCounter1);
-//  }
-//
-//  if (switchStateChange != 0) {
-//    screen.testHexPair(UI_INITIAL_OFFSET, UI_INITIAL_OFFSET + UI_HEXPAIR_Y_OFFSET, switchStateChange);
-//  }
-//}
-//
-//int8_t rotaryCounter2 = 0;
-//
-//void handleDebugEncoder2(RotaryEncoder& encoder) {
-//  int8_t rotaryStateChange = encoder.getRotaryStateChange();
-//  int8_t switchStateChange = encoder.getSwitchStateChange();
-//
-//  if (rotaryStateChange != 0) {
-//    rotaryCounter2 += rotaryStateChange;
-//    if (rotaryCounter2 < 0) {
-//      rotaryCounter2 = 0;
-//    }
-//    if (rotaryCounter2 > 0xFF) {
-//      rotaryCounter2 = 0xFF;
-//    }
-//    screen.testHexPair(UI_INITIAL_OFFSET + UI_HEXPAIR_X_OFFSET, UI_INITIAL_OFFSET, rotaryCounter2);
-//  }
-//
-//  if (switchStateChange != 0) {
-//    screen.testHexPair(UI_INITIAL_OFFSET + UI_HEXPAIR_X_OFFSET, UI_INITIAL_OFFSET + UI_HEXPAIR_Y_OFFSET, switchStateChange);
-//  }
-//}
-
 void loop() {
   uiStateManager.update();
-  //handleDebugEncoder1(rotaryEncoder1);
-  //handleDebugEncoder2(rotaryEncoder2);
 }
